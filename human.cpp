f97680fcea78da1e8b16a911c46d9675533e3f8e
AdelaCorbeanu <92207857+AdelaCorbeanu@users.noreply.github.com>
2022-04-13 10:43:25 +0300

    class human

---
+++ human.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adela on 4/13/2022.
+//
+
+#include "human.h"
---
+++ human.h
@@ -0,0 +1,14 @@
+//
+// Created by adela on 4/13/2022.
+//
+
+#ifndef OOP_HUMAN_H
+#define OOP_HUMAN_H
+
+
+class human {
+
+};
+
+
+#endif //OOP_HUMAN_H
